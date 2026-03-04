360cea8b91720fb3f3bb70c77996c338605e2952
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-03-04 22:58:02 +0530

    Update ai_summry.js

--- src/app/movie_server/ai_summry.js
+++ src/app/movie_server/ai_summry.js
@@ -1,20 +1,32 @@
 "use server"
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 import { configDotenv } from "dotenv";
-const groq = new Groq({ apiKey:`${process.env.GROQ_API_KEY}`});
+
 configDotenv()
-export async function description(que){
+const groq = new Groq({ apiKey:`${process.env.GROQ_API_KEY}`});
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY});
+ export async function description(que){
 let baseMessages = [
-    { 
+    {
       role: 'system',
-      content: `You are a movie summarizer.
-Your job is to explain the movie story in simple English.
-Rules:
-1. Write a detailed explanation of the movie.
-2. Use simple English words.
-3. Write at least 100-150 words.
-4. Explain the story from beginning to end.
-5. Response should be descriptive and clear. `
+      content: `You are a movie story summarizer.
+
+Your job is to explain the story of a movie in simple English.
+
+Instructions:
+- If you do not know the movie or the movie is very new, call the "websearch" tool to find information about the movie.
+- Use the search results to understand the movie story.
+
+Rules for the final answer:
+- Write 100-200 words.
+- Use simple and clear English.
+- Explain the story like a short narrative.
+- Start with the main characters and setting.
+- Then explain the main conflict and events.
+- End with the outcome or theme of the movie.
+-Write the response in one clear paragraph.
+`
     },
         {
       role: 'user', 
@@ -22,12 +34,56 @@ Rules:
     },
   
    ];
- 
-   const completion = await groq.chat.completions.create({ 
-    model: "llama-3.3-70b-versatile",
-    messages: baseMessages,             
-   }) 
- return completion.choices[0].message.content;
+
+    const completion = await groq.chat.completions.create({ 
+   model: "llama-3.1-8b-instant",
+    messages: baseMessages,              
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "websearch",
+          description: "search latest info",
+          parameters: {
+            type: "object",
+            properties: {
+              query: { type: "string" }
+            },
+            required: ["query"]
+          }
+        }
+      }
+    ],
+    tool_choice: "auto"
+  });
+
+let tool_calls=completion.choices[0].message.tool_calls;
+  if(!tool_calls){
+   return completion.choices[0].message.content ;
+  }
+    if(tool_calls){
+   for (const result of tool_calls) {
+  const arg = JSON.parse(result.function.arguments);
+  if (result.function.name === "websearch") {
+ const re = await websearch(arg);
+baseMessages.push({
+      role: "tool",
+      tool_call_id: result.id,
+      name: "websearch",
+      content: JSON.stringify(re)
+       });
+    const step2 = await groq.chat.completions.create({
+    model: "llama-3.1-8b-instant",
+    messages: baseMessages
+  });
+  return step2.choices[0].message.content;
+   }
 }
-// main("bahubali Full Movie in hinglish")
-// console.log(await description("bahibali"))
+ }
+}
+async function websearch({query}) {
+  console.log(query,"........")
+  const response = await tvly.search(query);
+  let res = response.results.map((item)=>{return item.content}).join("\n\n")
+  return res;
+}
