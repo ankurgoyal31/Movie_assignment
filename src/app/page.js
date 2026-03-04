43151c378cc239cdf51c088f6e62bebb44a45d3b
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-03-04 17:44:06 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,10 @@ export default function Page() {
   const[movies,set_movies] = useState([])
   const[loader,set_loader] = useState(true)
   const[err,set_err] = useState(false)
+  const[show_back,set_show_back] = useState(false)
   const[search_err,set_search_err] = useState(false)
    async function get() {
-    try{
+     try{
       let  arr = [1,2,3,4,5,6,7,8,9,10];
       let random = Math.floor(Math.random()*arr.length)
       console.log(random)
@@ -30,22 +31,30 @@ export default function Page() {
     router.push(`/movie_info?id=${id}`)
    }
    useEffect(() => {
-     get()
+       get()
   }, [])
 
   const find = async() =>{
      try{
       console.log("call")
+     set_err(false)
      let data=await get_search_movie(search_movie);
-     if(!data.sucess && data.data.length===0){
+      if(!data.sucess && data.data.length===0){
        set_search_err(true)
        set_loader(false);
        set_err(false)
        set_movies([])
+       set_show_back(true)
+       setTimeout(() => {
+       set_show_back(false)
+       set_search_err(false)
+       get()
+       },2000);
      }else{
       console.log("all movies")
       console.log(data)
       set_movies(data.data)
+      set_show_back(true)
      }
       }catch(err){
 console.log("nothing....")
@@ -65,8 +74,10 @@ console.log("nothing....")
      {loader && <><h3 style={{color:'white',justifySelf:'center'}}>loading....</h3></>}
     {err && <><h3 style={{color:'white',justifySelf:'center'}}>check your connection....</h3></>}
     {search_err && <><h3 style={{color:'white',justifySelf:'center'}}>invalid search....</h3></>}
-     <h1 className="all-populer-movies" style={{color:'white'}}>Popular Movies</h1>
-      <div className="design_movie">
+    {!show_back && <h1 className="all-populer-movies" style={{color:'white'}}>Popular Movies</h1>}
+    {show_back && <span><a style={{color:'white',textDecoration:'none',marginTop:'10%'}} href="/">⬅ Back</a> </span>}
+
+      <div style={{marginTop:'10px'}} className="design_movie">
         { movies.map((movie) => (
           <div onClick={()=>ai_summry(movie.id)} className="each-movie" key={movie.id}>
             <img className="image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}height="250" width="200"/>
